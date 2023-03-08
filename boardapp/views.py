96baa3b553401e20b47351c4dd275a0b030a2e95
BigMcQueen <mmmrym10@gmail.com>
2023-03-08 19:47:50 +0900

    ユーザー名の重複を防ぐ

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 
 def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'boardapp/signup.html', {'name': 'Tom'})
+        except IntegrityError:
+            return render(request, 'boardapp/signup.html', {'error': 'このユーザーはすでに登録されています！'})
+
     return render(request, 'boardapp/signup.html', {'name': 'Tom'})
