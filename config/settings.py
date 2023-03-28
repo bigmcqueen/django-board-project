f91dad60cdae5f8fe9d883e37ef7ab9c5f5a5ea9
BigMcQueen <mmmrym10@gmail.com>
2023-03-28 23:21:25 +0900

    Create top page

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'board.apps.BoardConfig',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +68,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'board.apps.BoardConfig',
             ],
         },
     },
