114bc0a9f18cded0ef124afcfc1a3186f6f181cb
BigMcQueen <mmmrym10@gmail.com>
2023-03-29 00:49:07 +0900

    Modify SignUp function

--- board/views.py
+++ board/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 from .forms import *
 
 # Create your views here.
 def top(request):
-    return render(request, 'board/top.html')
+    template_name = 'board/top.html'
+    return render(request, template_name)
 
 def signup(request):
-    if request.method == 'POST':
-        print('POST')
-        return render(request, 'board/signup.html', {'form': BoardForm})
-    else:
-        print('NOT POST')
-        return render(request, 'board/signup.html', {'form': BoardForm})
-    return render(request, 'board/signup.html', {'form': BoardForm})
+    template_name = 'board/signup.html'
+    form = BoardForm(request.POST or None)
+    ctx = {'form': form}
+    # フォームの送信時にエラーが無いかをチェックする
+    if form.is_valid():
+        # cleaned_data[フィールド名]で値をそれぞれ変数へ入れていく
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        # User.objects.create_user()を実行した時点で、userは作成される
+        user = User.objects.create_user(name, email, password)
+    return render(request, template_name, ctx)
