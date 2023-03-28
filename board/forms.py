d2e080bcc25e9646d473b136a52618f0badd24a3
BigMcQueen <mmmrym10@gmail.com>
2023-03-29 08:03:03 +0900

    Prohibit duplicate users

--- board/forms.py
+++ board/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class BoardForm(forms.Form):
-    name = forms.CharField(max_length=30, label='name')
-    email = forms.CharField(label='email')
-    password = forms.CharField(label='password', widget=forms.PasswordInput(), min_length=8)
+class SignUpForm(forms.Form):
+    name = forms.CharField(label='name', max_length=30, required=True)
+    email = forms.EmailField(label='email', required=True)
+    password = forms.CharField(label='password', widget=forms.PasswordInput(), min_length=8, required=True)
--- board/views.py
+++ board/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 from .forms import *
 
@@ -10,14 +11,19 @@ def top(request):
 
 def signup(request):
     template_name = 'board/signup.html'
-    form = BoardForm(request.POST or None)
-    ctx = {'form': form}
+    form = SignUpForm(request.POST or None)
+    params = {'form': form}
     # フォームの送信時にエラーが無いかをチェックする
     if form.is_valid():
         # cleaned_data[フィールド名]で値をそれぞれ変数へ入れていく
         name = form.cleaned_data['name']
         email = form.cleaned_data['email']
         password = form.cleaned_data['password']
-        # User.objects.create_user()を実行した時点で、userは作成される
-        user = User.objects.create_user(name, email, password)
-    return render(request, template_name, ctx)
+        try:
+            # User.objects.create_user()を実行した時点で、userは作成される
+            user = User.objects.create_user(name, email, password)
+        except IntegrityError:
+            params['context'] = 'このユーザーはすでに登録されています'
+            return render(request, template_name, params)
+
+    return render(request, template_name, params)
