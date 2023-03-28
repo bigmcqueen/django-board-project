a3fbd8a24b4c6b109dbcc99b69b40ef4a215be1a
BigMcQueen <mmmrym10@gmail.com>
2023-03-29 00:07:09 +0900

    Check POST Method

--- board/views.py
+++ board/views.py
@@ -6,4 +6,10 @@ def top(request):
     return render(request, 'board/top.html')
 
 def signup(request):
+    if request.method == 'POST':
+        print('POST')
+        return render(request, 'board/signup.html', {'form': BoardForm})
+    else:
+        print('NOT POST')
+        return render(request, 'board/signup.html', {'form': BoardForm})
     return render(request, 'board/signup.html', {'form': BoardForm})
