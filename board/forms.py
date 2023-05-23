7137abf3f3abdc6ffa023ac9832fcb71ddcce93d
BigMcQueen <mmmrym10@gmail.com>
2023-05-23 20:26:14 +0900

    update view

--- board/forms.py
+++ board/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
-class SignInAndSignUpForm(forms.Form):
-    email = forms.EmailField(label='email', required=True)
-    password = forms.CharField(label='password', widget=forms.PasswordInput(), min_length=8, required=True)
+from .models import Board
+
+
+class BoardForm(forms.ModelForm):
+    class Meta:
+        model = Board
+        fields = ['name', 'content']
+        widgets = {
+            'name': forms.TextInput(),
+            'content': forms.Textarea()
+        }
--- board/models.py
+++ board/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class BoardModel(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(max_length=200)
-    author = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Board(models.Model):
+    name = models.CharField('お名前', max_length=50)
+    content = models.TextField('内容', max_length=140)
+
+    def __str__(self):
+        return self.name
